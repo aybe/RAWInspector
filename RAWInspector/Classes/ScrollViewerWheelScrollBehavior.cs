64a4e243bec8e4aa93dfbe473b35b3344b17e190
Aybe <aybe@users.noreply.github.com>
2018-03-19 03:22:33 +0100

    Splitted wheel behaviors

---
+++ RAWInspector/Classes/ScrollViewerWheelScrollBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using JetBrains.Annotations;
+
+namespace RAWInspector.Classes
+{
+    public sealed class ScrollViewerWheelScrollBehavior : ScrollViewerWheelBehavior
+    {
+        public static readonly DependencyProperty HorizontalModifierProperty = DependencyProperty.Register(
+            nameof(HorizontalModifier), typeof(ModifierKeys), typeof(ScrollViewerWheelScrollBehavior),
+            new PropertyMetadata(ModifierKeys.Shift));
+
+        public static readonly DependencyProperty VerticalModifierProperty = DependencyProperty.Register(
+            nameof(VerticalModifier), typeof(ModifierKeys), typeof(ScrollViewerWheelScrollBehavior),
+            new PropertyMetadata(ModifierKeys.None));
+
+        [PublicAPI]
+        public ModifierKeys HorizontalModifier
+        {
+            get => (ModifierKeys) GetValue(HorizontalModifierProperty);
+            set => SetValue(HorizontalModifierProperty, value);
+        }
+
+        [PublicAPI]
+        public ModifierKeys VerticalModifier
+        {
+            get => (ModifierKeys) GetValue(VerticalModifierProperty);
+            set => SetValue(VerticalModifierProperty, value);
+        }
+
+        private bool CanHorizontal => Keyboard.Modifiers == HorizontalModifier;
+
+        private bool CanVertical => Keyboard.Modifiers == VerticalModifier;
+
+        protected override bool CanExecute()
+        {
+            return CanHorizontal || CanVertical;
+        }
+
+        protected override void Execute(ScrollViewer viewer, int delta)
+        {
+            if (viewer == null)
+                throw new ArgumentNullException(nameof(viewer));
+
+            var h = CanHorizontal;
+            var v = CanVertical;
+
+            var extents = h ? viewer.ScrollableWidth : viewer.ScrollableHeight;
+            var offset1 = h ? viewer.HorizontalOffset : viewer.VerticalOffset;
+            var offset2 = Math.Max(0.0d, Math.Min(extents, offset1 + delta * 48));
+
+            if (h)
+            {
+                viewer.ScrollToHorizontalOffset(offset2);
+            }
+            else if (v)
+            {
+                viewer.ScrollToVerticalOffset(offset2);
+            }
+        }
+    }
+}
---
+++ RAWInspector/Classes/ScrollViewerWheelZoomBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using JetBrains.Annotations;
+
+namespace RAWInspector.Classes
+{
+    public sealed class ScrollViewerWheelZoomBehavior : ScrollViewerWheelBehavior
+    {
+        public static readonly DependencyProperty ZoomingModifierProperty = DependencyProperty.Register(
+            nameof(ZoomingModifier), typeof(ModifierKeys), typeof(ScrollViewerWheelZoomBehavior),
+            new PropertyMetadata(ModifierKeys.Control));
+
+        [PublicAPI]
+        public ModifierKeys ZoomingModifier
+        {
+            get => (ModifierKeys) GetValue(ZoomingModifierProperty);
+            set => SetValue(ZoomingModifierProperty, value);
+        }
+
+        protected override bool CanExecute()
+        {
+            return Keyboard.Modifiers == ZoomingModifier;
+        }
+
+        protected override void Execute(ScrollViewer viewer, int delta)
+        {
+            if (viewer == null)
+                throw new ArgumentNullException(nameof(viewer));
+
+            if (!(viewer.Content is FrameworkElement element))
+                throw new ArgumentNullException(nameof(element));
+
+            if (!(element.LayoutTransform is ScaleTransform transform))
+                throw new ArgumentNullException(nameof(transform));
+
+            var scaleX = Math.Max(1.0d, Math.Min(9999.0d, transform.ScaleX - delta));
+            var scaleY = Math.Max(1.0d, Math.Min(9999.0d, transform.ScaleY - delta));
+
+            transform.ScaleX = scaleX;
+            transform.ScaleY = scaleY;
+        }
+    }
+}
