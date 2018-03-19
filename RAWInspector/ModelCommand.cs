0db185aece86c6fde94da3c07790adfa1f280667
Aybe <aybe@users.noreply.github.com>
2018-03-19 19:34:09 +0100

    Added model command classes

---
+++ RAWInspector/ModelCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace RAWInspector
+{
+    internal sealed class ModelCommand : RelayCommand
+    {
+        public ModelCommand(Action execute, bool keepTargetAlive = false)
+            : base(execute, keepTargetAlive)
+        {
+        }
+
+        public ModelCommand(Action execute, Func<bool> canExecute, bool keepTargetAlive = false)
+            : base(execute, canExecute, keepTargetAlive)
+        {
+        }
+    }
+
+    internal sealed class ModelCommand<T> : RelayCommand<T>
+    {
+        public ModelCommand(Action<T> execute, bool keepTargetAlive = false)
+            : base(execute, keepTargetAlive)
+        {
+        }
+
+        public ModelCommand(Action<T> execute, Func<T, bool> canExecute, bool keepTargetAlive = false)
+            : base(execute, canExecute, keepTargetAlive)
+        {
+        }
+    }
+}
