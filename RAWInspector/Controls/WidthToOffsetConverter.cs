a936688a828c1fe19e796c39d380c863d2d5e0c8
Aybe <aybe@users.noreply.github.com>
2018-03-26 19:04:15 +0200

    Enhanced line movement, now with 10 pixels at once

---
+++ RAWInspector/Controls/WidthToOffsetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RAWInspector.Controls
+{
+    internal sealed class WidthToOffsetConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is int i1 && values[1] is int i2)
+                return i1 * i2;
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
