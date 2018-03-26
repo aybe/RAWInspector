c3971a3b3078e41977ac8c9178f8bd2e959f1819
Aybe <aybe@users.noreply.github.com>
2018-03-26 22:07:47 +0200

    Implemented larger numeric TextBox with validation

---
+++ RAWInspector/Controls/IntegerValidationRule.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace RAWInspector.Controls
+{
+    internal sealed class IntegerValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string s && int.TryParse(s, out var _))
+                return ValidationResult.ValidResult;
+
+            return new ValidationResult(false, "Please enter an integer.");
+        }
+    }
+}
